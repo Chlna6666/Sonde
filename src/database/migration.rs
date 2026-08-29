6369b01393c99875aa642af2aa96dee4b0bcfb7e
Chlna6666 <79357769+Chlna6666@users.noreply.github.com>
2026-08-29 12:01:47 +0800

    fix: 注册旧 Histogram 数据修复迁移

--- src/database/migration.rs
+++ src/database/migration.rs
@@ -11,6 +11,7 @@ mod first_seen_index;
 mod job_leases;
 mod log_error_rollups;
 mod metrics_v2;
+mod metrics_v2_legacy_histogram;
 mod rollup_generation;
 mod tables;
 mod user_rollup_chunks;
@@ -27,6 +28,7 @@ use first_seen_index::FirstSeenIndex;
 use job_leases::JobLeases;
 use log_error_rollups::LogErrorRollups;
 use metrics_v2::MetricsV2;
+use metrics_v2_legacy_histogram::MetricsV2LegacyHistogramRepair;
 use rollup_generation::RollupGeneration;
 use tables::{
     create_alert_tables, create_application_tables, create_identity_tables, create_import_tables,
@@ -60,6 +62,7 @@ impl MigratorTrait for Migrator {
             Box::new(DirtySourceMask),
             Box::new(LogErrorRollups),
             Box::new(MetricsV2),
+            Box::new(MetricsV2LegacyHistogramRepair),
         ]
     }
 }
