3e5ec5bca859e402d9c52a36395a8b259fcb3648
Chlna6666 <79357769+Chlna6666@users.noreply.github.com>
2026-08-31 17:34:07 +0800

    refactor: simplify backup validation module

--- src/database/backup_validation.rs
+++ src/database/backup_validation.rs
@@ -1,8 +1,3 @@
-use super::{
-    backup_archive_repo as backup_v2_repo,
-    backup_archive_restore_repo as backup_v2_restore_repo,
-};
-
 #[path = "backup_archive_validation_impl.rs"]
 mod implementation;
 
